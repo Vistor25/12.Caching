1c2782ad34a75cfb9b5171483b33f9ac253671d9
Vistor25 <victorvoronko@gmail.com>
2018-01-12 00:58:58 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+using System.Runtime.Caching;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        MemoryCache cache = MemoryCache.Default;
+        public int Fibonachi(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+
+           
+            
+            if(cache.Get(n.ToString()) != null)
+            {
+                return (int)cache.Get(n.ToString());
+            }
+           
+            int number = Fibonachi(n - 1) + Fibonachi(n - 2);
+            cache.Add(n.ToString(), number, ObjectCache.InfiniteAbsoluteExpiration);
+            return number;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 fb = new Class1();
+            for(int i = 0; i<=1000; i++)
+            {
+                Console.WriteLine(fb.Fibonachi(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
